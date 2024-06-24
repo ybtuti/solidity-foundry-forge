db5d128152c91e8f69b55a68eabc802d8648cee9
0xodus <samuelmsuu@gmail.com>
2024-06-24 17:21:33 +0300

    chore: Add HelperConfig contract for deploying FundMe contract

--- foundry-fund-me/script/DeployFundMe.s.sol
+++ foundry-fund-me/script/DeployFundMe.s.sol
@@ -4,9 +4,12 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig();
+
         vm.startBroadcast();
         FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
