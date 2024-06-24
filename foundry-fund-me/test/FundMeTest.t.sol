915872f20d939438110e1ccff00e6541415cb20b
0xodus <samuelmsuu@gmail.com>
2024-06-24 15:35:07 +0300

    chore: Add FundMeTest contract for unit testing

---
+++ foundry-fund-me/test/FundMeTest.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+}
