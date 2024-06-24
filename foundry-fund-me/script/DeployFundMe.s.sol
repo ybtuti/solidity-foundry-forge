8d35f8fbe5f42c9b7322c22ac736f19ea6b1b9ae
0xodus <samuelmsuu@gmail.com>
2024-06-24 17:27:44 +0300

    chore: Update FundMe contract to use active network price feed

--- foundry-fund-me/script/DeployFundMe.s.sol
+++ foundry-fund-me/script/DeployFundMe.s.sol
@@ -9,9 +9,10 @@ import {HelperConfig} from "./HelperConfig.s.sol";
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
         HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
 
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
