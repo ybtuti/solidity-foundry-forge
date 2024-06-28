1bdbd3e346548dc255ff07855dd1cd9053856a18
ybtuti <samuelmsuu@gmail.com>
2024-06-28 11:59:34 +0300

    feat: Add multiple withdrawal functions to FundMe contract
    The `FundMe` contract now includes multiple withdrawal functions, such as `cheaperWithdrawal()` and `withdraw()`, allowing for different withdrawal scenarios. This enhances the flexibility and functionality of the contract.

--- foundry-fund-me/test/FundMeTest.t.sol
+++ foundry-fund-me/test/FundMeTest.t.sol
@@ -12,6 +12,7 @@ contract FundMeTest is Test {
     address USER = makeAddr("user");
     uint256 constant SEND_VALUE = 0.1 ether;
     uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
 
     function setUp() external {
         // fundMe = new FundMe();
@@ -76,8 +77,14 @@ contract FundMeTest is Test {
         uint256 startingOwnerBalance = fundMe.getOwner().balance;
         uint256 startingFundMeBalance = address(fundMe).balance;
         //Act
+        uint256 gasStart = gasleft();
+        vm.txGasPrice(GAS_PRICE);
         vm.prank(fundMe.getOwner());
         fundMe.withdraw();
+
+        uint256 gasEnd = gasleft();
+        uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+        console.log("Gas used: ", gasUsed);
 
         //Assert
         uint256 endingOwnerBalance = fundMe.getOwner().balance;
@@ -102,6 +109,29 @@ contract FundMeTest is Test {
 
         vm.startPrank(fundMe.getOwner());
         fundMe.withdraw();
+        vm.stopPrank();
+
+        //assert
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+    }
+
+    function testWithdrawFromMultipleFundersCheaper() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdrawal();
         vm.stopPrank();
 
         //assert
