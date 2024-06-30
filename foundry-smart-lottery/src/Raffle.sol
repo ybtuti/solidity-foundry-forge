547860a7d9dffe3b8cadcd11f9375cc8d8e57d04
ybtuti <samuelmsuu@gmail.com>
2024-06-30 13:06:51 +0300

    refactor: Update Raffle contract to include entrance fee validation
    The Raffle contract has been updated to include entrance fee validation in the enterRaffle function. This ensures that the user must send the correct amount of entrance fee to participate in the raffle. The validation is done by checking if the sent value is less than the entrance fee, and reverting the transaction if it is. This improves the functionality and security of the contract.

--- foundry-smart-lottery/src/Raffle.sol
+++ foundry-smart-lottery/src/Raffle.sol
@@ -1,3 +1,24 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
@@ -9,9 +30,27 @@ pragma solidity ^0.8.19;
  * @dev Impliment chainlink VRF
  */
 contract Raffle {
-    uint256 private entranceFee;
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle();
 
-    function enterRaffle() public {}
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        //require(msg.value >= i_entranceFee, "Not Enough entrance fee");
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        //require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());
+    }
 
     function pickWinner() public {}
+
+    /** Getter functions */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
 }
