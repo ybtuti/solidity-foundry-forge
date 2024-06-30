855a453afd87f60c06d4ba3525c5a2be0569921c
ybtuti <samuelmsuu@gmail.com>
2024-06-30 12:49:30 +0300

    refactor: Update submodule commit reference in html-fund-me
    Update the submodule commit reference in html-fund-me from f6b40c8 to b468c76. This ensures that the correct version of the submodule is being used in the project.

---
+++ foundry-smart-lottery/src/Raffle.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+/**
+ * @title Raffle
+ * @author 0xodus
+ * @notice This contract creates a raffle
+ * @dev Impliment chainlink VRF
+ */
+contract Raffle {
+    uint256 private entranceFee;
+
+    function enterRaffle() public {}
+
+    function pickWinner() public {}
+}
