2a75f51482374c52c95586ff2cedafc55b153fd1
ybtuti <samuelmsuu@gmail.com>
2024-07-02 19:25:04 +0300

    refactor: Add test for preventing players from entering when raffle is calculating

--- foundry-smart-lottery/test/unit/RaffleTest.t.sol
+++ foundry-smart-lottery/test/unit/RaffleTest.t.sol
@@ -70,4 +70,18 @@ contract RaffleTest is Test {
         //assert
         raffle.enterRaffle{value: entranceFee}();
     }
+
+    function testDontAllowPlayersToEnterWhenRaffleIsCalculating() public {
+        //Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        raffle.performUpkeep("");
+
+        //Act / Assert
+        vm.expectRevert(Raffle.Raffle__RaffleNotOpen.selector);
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+    }
 }
