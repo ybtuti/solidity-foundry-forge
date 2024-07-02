12e3275c01b10ec3d9c577cfc109a73d54221352
ybtuti <samuelmsuu@gmail.com>
2024-07-02 19:59:36 +0300

    refactor: Include CreateSubscription contract for subscription creation in DeployRaffle Script

---
+++ foundry-smart-lottery/script/Interactions.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+
+contract CreateSubscription is Script {
+    function createSubscriptionUsingConfig() public returns (uint256, address) {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+        (uint256 subId, ) = createSubscription(vrfCoordinator);
+        return (subId, vrfCoordinator);
+
+        //Create Subscription
+    }
+
+    function createSubscription(
+        address vrfCoordinator
+    ) public returns (uint256, address) {
+        console.log("Creating subscription on chain Id:", block.chainid);
+        vm.startBroadcast();
+        uint256 subId = VRFCoordinatorV2_5Mock(vrfCoordinator)
+            .createSubscription();
+        vm.stopBroadcast();
+
+        console.log("Your subscription Id is:", subId);
+        console.log(
+            "Please update the subscription Id in your HelperConfig.s.sol"
+        );
+        return (subId, vrfCoordinator);
+    }
+
+    function run() public {}
+}
