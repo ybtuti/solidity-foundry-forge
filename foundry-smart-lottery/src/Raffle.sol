414a47229289de4a8851de3bec85ee3040b6c7c7
ybtuti <samuelmsuu@gmail.com>
2024-07-02 19:06:52 +0300

    refactor: Add getPlayer function to Raffle contract

--- foundry-smart-lottery/src/Raffle.sol
+++ foundry-smart-lottery/src/Raffle.sol
@@ -174,4 +174,8 @@ contract Raffle is VRFConsumerBaseV2Plus {
     function getRaffleState() external view returns (RaffleState) {
         return s_raffleState;
     }
+
+    function getPlayer(uint256 indexOfPlayer) external view returns (address) {
+        return s_players[indexOfPlayer];
+    }
 }
