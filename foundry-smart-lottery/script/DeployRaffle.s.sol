b8335121dba9aecd64c36ea940c71fbb3b1de77a
ybtuti <samuelmsuu@gmail.com>
2024-07-02 19:58:53 +0300

    refactor: Update DeployRaffle Script to include subscription creation
    Update the DeployRaffle script to include the creation of a subscription if the subscription ID is not already set in the network configuration. This change ensures that a subscription is created for the Raffle contract to interact with the VRF coordinator. By checking if the subscription ID is 0, the script creates a new instance of the CreateSubscription contract and calls the createSubscription function to generate a new subscription ID and set the VRF coordinator address. This enhances the functionality and reliability of the Raffle contract deployment process.

--- foundry-smart-lottery/script/DeployRaffle.s.sol
+++ foundry-smart-lottery/script/DeployRaffle.s.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
+import {CreateSubscription} from "script/Interactions.s.sol";
 
 contract DeployRaffle is Script {
     function run() public {}
@@ -12,6 +13,13 @@ contract DeployRaffle is Script {
     function deployContract() public returns (Raffle, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig();
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
+        if (config.subscriptionId == 0) {
+            //Create Subscription
+            CreateSubscription createSubscription = new CreateSubscription();
+            (config.subscriptionId, config.vrfCoordinator) = createSubscription
+                .createSubscription(config.vrfCoordinator);
+        }
 
         vm.startBroadcast();
         Raffle raffle = new Raffle(
