f56b00cb89b14d386a8f3e71455e127d4c14428f
ybtuti <samuelmsuu@gmail.com>
2024-07-03 21:14:24 +0300

    refactor: Add getLastTimeStamp and getRecentWinner functions to Raffle contract
    This commit adds two new functions, getLastTimeStamp and getRecentWinner, to the Raffle contract in the foundry-smart-lottery project. The getLastTimeStamp function returns the last timestamp recorded in the contract, while the getRecentWinner function returns the address of the most recent winner. These functions provide additional information about the state of the Raffle contract and enhance the functionality of the smart lottery project.

--- foundry-smart-lottery/src/Raffle.sol
+++ foundry-smart-lottery/src/Raffle.sol
@@ -180,4 +180,12 @@ contract Raffle is VRFConsumerBaseV2Plus {
     function getPlayer(uint256 indexOfPlayer) external view returns (address) {
         return s_players[indexOfPlayer];
     }
+
+    function getLastTimeStamp() external view returns (uint256) {
+        return s_lastTimeStamp;
+    }
+
+    function getRecentWinner() external view returns (address) {
+        return s_recentWinner;
+    }
 }
