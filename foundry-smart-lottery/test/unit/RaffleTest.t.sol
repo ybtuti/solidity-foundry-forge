3b53f07a7ad0f807168d08b78ccffc1952567b36
ybtuti <samuelmsuu@gmail.com>
2024-07-03 21:15:07 +0300

    refactor: Add test for fulfilling random words, picking a winner, resetting, and sending money
    This commit adds a new test case to the RaffleTest contract in the foundry-smart-lottery project. The test case, testFulfillrandomWordsPicksAWinnerResetsAndSendsMoney, verifies the functionality of fulfilling random words, picking a winner, resetting the raffle state, and sending the prize money to the winner. The test case covers scenarios where additional entrants join the raffle, and ensures that the winner receives the correct prize amount. This enhancement improves the reliability and functionality of the smart lottery project.

--- foundry-smart-lottery/test/unit/RaffleTest.t.sol
+++ foundry-smart-lottery/test/unit/RaffleTest.t.sol
@@ -189,4 +189,48 @@ contract RaffleTest is Test {
             address(raffle)
         );
     }
+
+    function testFulfillrandomWordsPicksAWinnerResetsAndSendsMoney()
+        public
+        raffleEntered
+    {
+        //Arrange
+        uint256 additionalEntrants = 3;
+        uint256 startingIndex = 1;
+        address expectedWinner = address(1);
+
+        for (
+            uint256 i = startingIndex;
+            i < startingIndex + additionalEntrants;
+            i++
+        ) {
+            address newPlayer = address(uint160(i));
+            hoax(newPlayer, 1 ether);
+            raffle.enterRaffle{value: entranceFee}();
+        }
+        uint256 startingTimeStamp = raffle.getLastTimeStamp();
+        uint256 winnerStartingBalance = expectedWinner.balance;
+
+        //Act
+        vm.recordLogs();
+        raffle.performUpkeep("");
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        bytes32 requestId = entries[1].topics[1];
+        VRFCoordinatorV2_5Mock(vrfCoordinator).fulfillRandomWords(
+            uint256(requestId),
+            address(raffle)
+        );
+
+        //assert
+        address recentWinner = raffle.getRecentWinner();
+        Raffle.RaffleState raffleState = raffle.getRaffleState();
+        uint256 winnerBalance = recentWinner.balance;
+        uint256 endingTimeStamp = raffle.getLastTimeStamp();
+        uint256 prize = entranceFee * (additionalEntrants + 1);
+
+        assert(recentWinner == expectedWinner);
+        assert(uint256(raffleState) == 0);
+        assert(winnerBalance == winnerStartingBalance + prize);
+        assert(endingTimeStamp > startingTimeStamp);
+    }
 }
